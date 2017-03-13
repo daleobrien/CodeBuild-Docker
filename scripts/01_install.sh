b941981b48dc627d4503f73e1b516a10cffd171e
Dale O'Brien <dale@do.id.au>
2017-03-14 10:50:40 +1100

    Fixed installing when requirements.txt hasn't changed

--- scripts/01_install.sh
+++ scripts/01_install.sh
@@ -1,10 +1,11 @@
 echo "01 install"
 
-INSTALL="$(pip install -r requirements.txt  | grep -v 'Requirement already satisfied')"
-echo "$INSTALL"
+INSTALL="$(pip install -r requirements.txt  | grep -v 'Requirement already satisfied' | grep -v '^$')"
 
-NO_LINES=`echo "$INSTALL" | wc -l`
-if [ ! $NO_LINES -eq 0 ]; then
+echo "pip install -r requirements.txt ->> '$INSTALL'"
+
+NO_LINES=`echo "$INSTALL" |  grep -c '^'`
+if [ ! $NO_LINES -eq 1 ]; then
     echo "Requirements have changed, will rebuild."
     docker build .
 
